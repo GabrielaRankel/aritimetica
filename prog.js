683303f0866922c22bdf663cbc13c9fdde7dffa6
Gabriela <gabriela_rankel@estudante.sesisenai.org.br>
2025-03-18 11:49:03 -0300

    Programando a Soma De Dois Números

---
+++ prog.js
@@ -0,0 +1,9 @@
+let resposta = document.getElementById('resposta')
+
+function calcular(){
+    let num1 = (document.getElementById("num1").value)
+    let num2 = (document.getElementById("num2").value)
+    let soma = num1 + num2
+    resposta.innerHTML = 'Resltatdo da Soma =' + soma
+
+}
